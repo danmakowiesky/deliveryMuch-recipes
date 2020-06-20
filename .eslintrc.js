3e28dfe17c1db060deaa81bd1e161c1e743049c1
danielmakowiesky <dvm1993@hotmail.com>
2020-06-20 18:53:34 -0300

    ajustes eslint e dotenv adicionado

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,12 @@ module.exports = {
     extends: ['airbnb-base', 'prettier'],
     plugins: ['prettier'],
     parserOptions: {
-      ecmaVersion: 11,
+      ecmaVersion:2018,
       sourceType: 'module',
     },
     rules: {
       'prettier/prettier': 'error',
       'class-methods-use-this': 'off',
       'no-param-reassign': 'off',
-      camelcase: 'off',
-      'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
     },
   };
