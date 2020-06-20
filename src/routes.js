265c5c3f24e0c705284bdc0ec28830536309118e
danielmakowiesky <dvm1993@hotmail.com>
2020-06-20 14:46:39 -0300

    adicionando eslint e prettier - No Eslint foi adicionado o padrao de codigo do airbnb - E o prettier para formatar automaticamente de acordo com as regras

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-import {Router} from 'express' ;
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (request, response) => {
-  return response.json({message: 'Delivery Much, quero fazer parte do time de vocês!!!!'})
+  return response.json({
+    message: 'Delivery Much, quero fazer parte do time de vocês!!!!',
+  });
 });
 
 export default routes;
