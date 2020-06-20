785ca249fbc7495731743718547c5d7a4f7ea7c3
danielmakowiesky <dvm1993@hotmail.com>
2020-06-20 18:48:36 -0300

    consumindo api recipepuppy

--- src/services/GiphyAPI.js
+++ src/services/GiphyAPI.js
@@ -1,13 +1,27 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const api = axios.create({
-  baseURL: `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=`,
+  baseURL: 'https://api.giphy.com/v1/gifs/',
 });
 
 export default async function getGifRecipe(recipe) {
-  const q = `${recipe}`;
+  const q = `search?api_key=${process.env.GIPHY_API_KEY}&q=${recipe}`;
 
   const res = await api.get(q);
+  if (res.status !== 200) {
+    return {
+      error:
+        'The giphy api is not responding at the moment, please try again later',
+    };
+  }
 
-  return res.data[0].url;
+  if (res.data.data.results.length === 0)
+    return {
+      error: 'No gif found based on your search',
+    };
+
+  return res.data.data[0].url;
 }
--- src/services/PuppyAPI.js
+++ src/services/PuppyAPI.js
@@ -1,11 +1,33 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://www.recipepuppy.com/api/?i=',
+  baseURL: 'http://www.recipepuppy.com/api/',
 });
 
 export default async function recipes(i) {
-  const res = await api.get(i);
+  const res = await api.get(`?i=${i}`);
 
-  return res;
+  if (res.status !== 200) {
+    return {
+      error:
+        'The recipepuppy api is not responding at the moment, please try again later',
+    };
+  }
+
+  if (res.data.results.length === 0)
+    return {
+      error: 'There are no recipes for these researched ingredients',
+    };
+
+  // eslint-disable-next-line no-shadow
+  const recipes = res.data.results.map((recipe) => {
+    // eslint-disable-next-line no-shadow
+    const i = recipe.ingredients.split(',');
+    return {
+      title: recipe.title,
+      href: recipe.href,
+      igredients: i.sort(),
+    };
+  });
+  return recipes;
 }
